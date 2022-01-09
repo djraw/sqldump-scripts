5c6a5af4305316608c158c2ec7994348d32d4f26
R.A.W <raw1@gmx.de>
2022-01-09 15:18:30 +0100

    Update db-backup-remote.sh

--- db-backup-remote.sh
+++ db-backup-remote.sh
@@ -7,7 +7,7 @@ bakpath=<FILESYSTEMPATH>
 prefix=<PREFIX>
 deleteolderthandays=14
 
-mysqldump -h $host -u $dbuser --password=$dbpwd --all-databases | gzip > $bakpath/$prefix-$(date +%Y-%m-%d_%H-%M).sql.gz
+mysqldump -h $host -u $dbuser --password=$dbpwd --all-databases | xz > $bakpath/$prefix-$(date +%Y-%m-%d_%H-%M).sql.xz
 
 find $bakpath/ -name $prefix*.sql.gz -type f -mtime +$deleteolderthandays -delete
 
